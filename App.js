e8b5a3e0e597816c8a009ef7e061583ebd630b22
SirHauler <realalexalvarado@gmail.com>
2023-02-22 22:56:49 -0800

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './components/homepage/home';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
+      {/* <StatusBar style="auto" /> */}
+
     </View>
   );
 }
---
+++ components/homepage/styles.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native'; 
+
+
+export default homeStyle = StyleSheet.create({
+    container: {
+        flex: 1, 
+    }
+})
